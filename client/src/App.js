9a9bdc5ef61c206e348de2c21ceab47aeaf11e86
Lucas Boinet <42383760+lucasboinet@users.noreply.github.com>
2022-05-02 14:56:32 +0200

    fix conflict

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,13 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.scss';
 
-<<<<<<< Updated upstream
-function App() {
-  return (
-    <>
-    </>
-=======
-import Homepage from '@/pages/Homepage/Homepage';
 import Details from '@/pages/Details/Details';
 
 function App() {
   return (
     <Routes>
-      <Route exact path="/" element={<Homepage />} />
       <Route exact path="/project/:id" element={<Details />} />
     </Routes>
->>>>>>> Stashed changes
   );
 }
 
