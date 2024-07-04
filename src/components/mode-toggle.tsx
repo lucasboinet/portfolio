dfc60a75fb50267b3d8648c04d0e69790fb01b2f
Lucas B <lucas@skyloud.fr>
2024-07-04 16:56:54 +0200

    first commit

---
+++ src/components/mode-toggle.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
+import { useTheme } from "next-themes";
+import React from "react";
+
+export function ModeToggle() {
+  const { theme, setTheme } = useTheme();
+
+  function handleSetTheme() {
+    setTheme(theme === "dark" ? "light" : "dark");
+  }
+
+  return (
+    <Button
+      variant="ghost"
+      type="button"
+      size="icon"
+      className="px-2"
+      onClick={handleSetTheme}
+    >
+      <SunIcon className="h-[1.2rem] w-[1.2rem] text-neutral-800 dark:hidden dark:text-neutral-200" />
+      <MoonIcon className="hidden h-[1.2rem] w-[1.2rem] text-neutral-800 dark:block dark:text-neutral-200" />
+    </Button>
+  );
+}
